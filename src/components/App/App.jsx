cefcfb3a778213afd06f754891a3ac70c3c398a3
Dmitry-Ship <vooxil@gmail.com>
2018-11-20 09:04:53 +0300

    logos

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,11 @@ import Btn from 'components/Btn'
 import Logos from 'components/Logos'
 import ClickCounter from 'components/ClickCounter'
 
-const createLogo = () => ({ inverse: !!Math.round(Math.random()) })
-
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ClickCounter />
+        <Logos />
       </div>
     )
   }
